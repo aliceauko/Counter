60dd51579107671356af4f6457ec172ea0d8692a
Alice-Owiti <aukoalice@gmail>
2022-12-08 19:16:17 +0300

    Done

---
+++ Assets/README.md
@@ -0,0 +1,50 @@
+# Counter
+*****
+### Author : Alice Auko 8th December 2022
+****
+## Project Description
+A Responsive Counter website created using html5 and css and vannila JavaScript
+
+******
+
+## SCREENSHOT
+![image](/Assets/Images/127.0.0.1_5500_%20(4).png)
+
+
+********
+## SetUp Instruction
+### Requirements
+* [HTML](lang Url)
+* Text editor eg [Visual Studio Code](https://code.visualstudio.com/download)
+
+
+
+### Getting Files
+* Fork the repo
+- Create a new branch in your terminal (git checkout -b improve-feature)
+- Install the prerequisites
+- Make appropriate changes in file(s)
+- Run the server to see the changes
+- Add the changes and commit them (git commit -am "Improve App")
+- Push to the branch (git push origin improve-app)
+- Create a Pull request
+* Open the folder location on terminal and run using the live server
+
+## Live Link
+Or you can access the web application directly via this [LINK.](https://counter-ten-fawn.vercel.app)
+*****
+
+## Technologies Used
+1. HTML
+2. CSS
+3. JavaScript
+
+*****
+
+## Contact Information
+* Email : aukoalice@gmail.com
+* Linked - [Owiti Alice Auko](https://www.linkedin.com/in/owiti-alice-auko-580b2818a)
+*****
+## [License](LICENSE)
+MIT License
+Copyright (c) 2022 Alice Auko
--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,23 @@
-(function(){
-    const buttons = document.querySelectorAll(".countBtn")
+(function () {
+  const buttons = document.querySelectorAll(".countBtn");
 
-    let count = 0
-    buttons.forEach(function(button){
-        button.addEventListener('click', function(){
-            if (button.classList.contains('decrease')){
-            count--
-            }
-            else
-            if (button.classList.contains('increase')){
-                count++
-            }
+  let count = 0;
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      if (button.classList.contains("decrease")) {
+        count--;
+      } else if (button.classList.contains("increase")) {
+        count++;
+      }
 
-            const counter = document.querySelector('.counter')
-            counter.textContent = count
+      const counter = document.querySelector(".counter");
+      counter.textContent = count;
 
-
-            if (count < 0){
-                counter.style.color ='red'
-            }
-           else if (count > 0){
-                counter.style.color ='green'
-            }
-            else counter.style.color ='black'
-        })
-    })
-}) ()
+      if (count < 0) {
+        counter.style.color = "red";
+      } else if (count > 0) {
+        counter.style.color = "green";
+      } else counter.style.color = "black";
+    });
+  });
+})();
